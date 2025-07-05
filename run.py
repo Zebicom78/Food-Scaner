0bc5e0b438ba4948d2e517066133dcd69c46d4cc
Zebicom78 <zebicomcomapny@gmail.com>
2025-07-05 12:11:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask
+from routes import main
+import os
 
-app = create_app()
+app = Flask(__name__)
+app.register_blueprint(main)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
